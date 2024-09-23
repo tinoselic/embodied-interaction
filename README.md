c342d7cfba80986de07f1bbd10b3abe7a360d37f
Tino Selic <tino.selic@gmail.com>
2024-09-23 17:28:41 +0300

    Files added.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Embodied Interaction Course Documentation
+
+Documentation website for the Embodied Interaction course (spring 2023) of the New Media major at Aalto University.
---
+++ app.js
@@ -0,0 +1,26 @@
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+const main = document.querySelector('main');
+
+openMenu.addEventListener('click',showNav);
+closeMenu.addEventListener('click',closeNav);
+
+function showNav() {
+    mainMenu.style.display = 'block';
+    openMenu.style.display = 'none';
+    main.style.display = 'none';
+}
+
+function closeNav() {
+    mainMenu.style.display = 'none';
+    openMenu.style.display = 'block';
+    main.style.display = 'block';
+}
+
+// Footer
+document.addEventListener("DOMContentLoaded", function() {
+    var footer = document.getElementById("copyright");
+    var footerText = "© 2024 Tino Selic";
+    footer.textContent = footerText;
+});
